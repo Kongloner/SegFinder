b1b90ee0a38ceb78bbe6d184a28e0f0085f8293b
Kongloner <37209207+Kongloner@users.noreply.github.com>
2024-12-08 22:47:21 +0800

    Update coefficient-matrix_pre.R

--- src/R/coefficient-matrix_pre.R
+++ src/R/coefficient-matrix_pre.R
@@ -1,5 +1,3 @@
-rm(list = ls())
-
 cat("Loading of the libraries.... \n")
 suppressMessages(library(stringr))
 suppressMessages(library(dplyr))
@@ -10,10 +8,10 @@ args <- commandArgs(T)
 setwd(args[2])
 
 data1 <- read.table(paste0(args[1],"_megahit_assemble_nr.edited.tsv"),sep = "\t",quote = "")
-data1 <- data1[,-(8:9)]
+data1 <- data1[,-1]
 colnames(data1) <- c("Contigs","Length","Accession","Species","Similarly","aa_length","E-value","Species_annotion")
 
-split_b<-str_split(data1$Species_annotion,"\\|")
+split_b<-str_split(data1$Species_annotion,"|")
 b<-sapply(split_b,"[",2)
 data1$Species_annotion <-b
 data1$Species_annotion[is.na(data1$Species_annotion)]<-"V"
@@ -23,3 +21,4 @@ data_res <- data2[which(data2$Length > as.numeric(args[3])), ]
 
 write.table(data_res,"RSEM_pre.txt",  sep="\t",  row.names=TRUE,
             col.names=TRUE, quote=TRUE)
+~
